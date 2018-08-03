c2509038468e0ec5b06f68403d3b078190ed0395
RAVI <ravi@ravis-mac-mini.local>
2018-08-03 18:52:48 +0530

    Initial Commit

---
+++ Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by RAVI on 03/08/18.
+//  Copyright © 2018 RAVI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
